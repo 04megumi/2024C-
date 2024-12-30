f744e88f528465d50201fad0164fe9f8d15ebe23
megumi <144149012+04megumi@users.noreply.github.com>
2024-12-30 22:32:25 +0800

    feat:finish all features and pass all tests.

--- src/CmderFactory.cpp
+++ src/CmderFactory.cpp
@@ -6,8 +6,15 @@ namespace adas
 CmderList CmderFactory::GetCmders(const std::string& commands) const noexcept
 {
     CmderList cmders;
-    
-    for(const auto cmd : commands){
+    bool flag = false;
+
+    for(auto cmd : commands){
+        if(cmd=='T')  flag = true;
+        if(cmd=='R' && flag)
+        {
+            cmd = 'Z';
+            flag = false;
+        }
         const auto it = cmderMap.find(cmd);
         if(it!=cmderMap.end()){
             cmders.push_back(it->second);
